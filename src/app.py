ceb1665b7df6c2193ad9ce77065d4ab62cd66e20
Vaishnav2607 <vaishnavnarigiri267@gmail.com>
2024-03-30 22:54:12 +0530

    Add basic frontend

--- src/app.py
+++ src/app.py
@@ -4,3 +4,12 @@ st.title("Chat with websites")
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
+
+
+user_query = st.chat_input("Type your message here")
+if user_query is not None and user_query !="":
+    with st.chat_message("Me"):
+        st.write("I want to know about your Langchain")
+
+    with st.chat_message("Jarvis"):
+        st.write("IDK IDK IDK IDK")
